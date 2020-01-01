7c04de43f490b199166b8e618d822118c0b22806
Longo <plongo@indracompany.com>
2020-01-01 09:40:52 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import routes from './routes';
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/instagram', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.listen(3333);
